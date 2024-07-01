b9a7bea80d3e19fe9ba2d82309bb3da7b7c78fbe
Carlos Garcia <carlosrjg7@gmail.com>
2024-07-01 17:16:34 -0500

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className='min-h-screen flex items-center justify-center'>
+        <h1 className='text-4xl font-bold text-blue-500'>
+          ¡Hola, adplaypro!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "~",
+        replacement: "/src",
+      },
+    ],
+  },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./test/setup.js",
+  },
+});
