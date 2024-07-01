aef3b84e568d812cf65d59074e36a4c0a4190b16
Carlos Garcia <carlosrjg7@gmail.com>
2024-07-01 17:43:45 -0500

    login init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import "./App.css";
+import "../styles/main.css";
 
 function App() {
   return (
     <>
       <div className='min-h-screen flex items-center justify-center'>
-        <h1 className='text-4xl font-bold text-blue-500'>
-          ¡Hola, adplaypro!
-        </h1>
+        <h1 className='text-4xl font-bold text-blue-500'>¡Hola, adplaypro!</h1>
       </div>
     </>
   );
